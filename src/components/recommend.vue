b0fd4332962a48e39f4bcccb11aa9c5532081aea
lemon-lexiaobai <17743692127@163.com>
2020-09-12 12:16:55 +0800

    appStore init

---
+++ src/components/recommend.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>{{msg}}</p>
+</template>
+
+<script>
+export default {
+  name: 'recommend',
+  data () {
+    return {
+      msg: 'Recommend'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
